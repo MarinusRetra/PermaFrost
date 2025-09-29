06a101d4898017b2c19a5280ffcff951b131c778
Tempy <usedtemplate@gmail.com>
2025-09-29 14:29:32 +0200

    Add initial Unity carriage generation setup
    Added base scripts, prefabs, and scene for carriage generation in Unity. Includes CarriageClass and Generation scripts, Room1 and Room2 prefabs, and CarriageGeneration scene. Also added VS Code configuration files for Unity development.

---
+++ Assets/Scripts/Carriage/CarriageClass.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CarriageClass : MonoBehaviour
+{
+    [Header("Transforms")]
+    [SerializeField] private Transform _entryPoint;
+    [SerializeField] private Transform _exitPoint;
+    [SerializeField] private Transform _mainObject;
+}
---
+++ Assets/Scripts/Carriage/Generation.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    [Header("Rooms")]
+    [SerializeField] private List<GameObject> _rooms;
+}

3c53be26e5c1ea51fb99932b88382e33818f6e21
Spectre <rpverschuren@st.noorderpoort.nl>
2025-10-06 14:19:32 +0200

    Working on All Ears movement
    -Temporary: NavMesh-StaticBaseGeneration
    
    -Added getting the corners of a room for all ears
    
    -Added a general monster class

---
+++ Assets/Scripts/Monsters/Monster.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Monster : MonoBehaviour
+    {
+        //This script can be used for any shared things between monsters
+        //Currently that is nothing.
+        //
+        //         :(
+    }
+}

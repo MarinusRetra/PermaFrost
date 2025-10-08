2185dc7ab33273e13c67dda3fa074c8c95b10cd9
ThunderBeans (Marinus) <103601175+ThunderBeans@users.noreply.github.com>
2025-10-08 22:07:44 +0200

    Changed the way the first and last submeshes swap for interactable objects

--- Assets/Scripts/InteractObject.cs
+++ Assets/Scripts/InteractObject.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering;
 
 namespace Gameplay
 {
@@ -18,9 +20,17 @@ namespace Gameplay
             _materials = GetComponent<MeshRenderer>().materials;
             if (_materials[^1].color == _outlineMat.color)
             {
-                //Zet de laatste material's zijn submesh naar dezelfde submesh als de eerste submesh.
                 Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-                mesh.SetSubMesh(mesh.subMeshCount - 1, mesh.GetSubMesh(0));
+                //mesh.SetSubMesh(mesh.subMeshCount - 1, mesh.GetSubMesh(0)); Oude deprecated manier om meshes te wisselen
+
+                //Zet de laatste material's zijn submesh naar dezelfde submesh als de eerste submesh.
+                List<SubMeshDescriptor> _descriptors = new();
+                for (int i = 0; i < mesh.subMeshCount; i++)
+                {
+                    _descriptors.Add(mesh.GetSubMesh(i));
+                }
+                _descriptors[^1] = _descriptors[0];
+                mesh.SetSubMeshes(_descriptors);
             }
         }
         public void Hover()

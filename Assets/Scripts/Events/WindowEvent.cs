b1e80828a80b04ad8bf6dcc3bfd2d5524b957363
Spectre <rpverschuren@st.noorderpoort.nl>
2025-11-06 15:17:55 +0100

    Added broken window event

--- Assets/Scripts/Events/WindowEvent.cs
+++ Assets/Scripts/Events/WindowEvent.cs
@@ -5,12 +5,21 @@ namespace Gameplay
     [CreateAssetMenu(menuName = "Events/BrokenWindowEvent")]
     public class WindowEvent : EventClass
     {
+        [SerializeField]
+        private GameObject _freezingPrefab;
         public override bool Entered(GameObject room)
         {
+            GameObject _freeze = Instantiate(_freezingPrefab);
+            _freeze.transform.parent = room.transform;
+            _freeze.transform.localScale = new Vector3(3,1,1);
+            _freeze.transform.position = room.transform.position;
+
+            //break windows visually
             return true;
         }
         public override bool Exited(GameObject room)
         {
+            //DO NOT DESPAWN
             return true;
         }
         public override bool Triggered(GameObject room)

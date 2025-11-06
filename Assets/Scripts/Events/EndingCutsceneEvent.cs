c4734ce94235850b60bc8618290350f3eec15561
Spectre <rpverschuren@st.noorderpoort.nl>
2025-11-06 11:09:57 +0100

    Added ending cutscene event

--- Assets/Scripts/Events/EndingCutsceneEvent.cs
+++ Assets/Scripts/Events/EndingCutsceneEvent.cs
@@ -5,8 +5,12 @@ namespace Gameplay
     [CreateAssetMenu(menuName = "Events/EndingEvent")]
     public class EndingCutsceneEvent : EventClass
     {
+        [SerializeField]
+        private GameObject _endingCutscenePrefab;
         public override bool Entered(GameObject room)
         {
+           GameObject cut =  Instantiate(_endingCutscenePrefab);
+            cut.transform.position = new Vector3(300, 300, 300);
             return true;
         }
         public override bool Exited(GameObject room)

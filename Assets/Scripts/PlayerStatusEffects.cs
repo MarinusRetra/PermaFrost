a48be8e89d88880ca91a35671c42c55707144055
Spectre <rpverschuren@st.noorderpoort.nl>
2025-11-11 10:29:52 +0100

    Added sliders for status effects

--- Assets/Scripts/PlayerStatusEffects.cs
+++ Assets/Scripts/PlayerStatusEffects.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatusEffects : MonoBehaviour
 {
     public static PlayerStatusEffects Instance;
 
     private PlayerHealth _playerHP;
+
+    [SerializeField] private Slider _insanitySlider;
+    [SerializeField] private Slider _freezingSlider;
 
     void Awake()
     {
@@ -27,13 +31,16 @@ public class PlayerStatusEffects : MonoBehaviour
     //Sanity related functions
     private IEnumerator HandleInsanity()
     {
+        _insanitySlider.maxValue = InsanityDeath;
         while (true)
         {
+            _insanitySlider.gameObject.SetActive(_currentInsanity == 0 ? false : true);
             if (_insanityCauses.Count > 0) { _currentInsanity += 2; }
-            if (_insanityCauses.Count == 0 && _currentInsanity > 0) { _currentInsanity--; }
+            if (_insanityCauses.Count == 0 && _currentInsanity > 0) { _currentInsanity -= 1; }
+            _insanitySlider.value = _insanitySlider.maxValue - _currentInsanity;
 
             if (_currentInsanity >= InsanityDeath) { _playerHP.GameOver(); }
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.2f);
         }
     }
     public void ManageInsanityCauses(string name, bool remove)
@@ -61,14 +68,17 @@ public class PlayerStatusEffects : MonoBehaviour
     //Frostbite related functions
     private IEnumerator HandleFrostbite()
     {
+        _freezingSlider.maxValue = FrostbiteDeath;
         while (true)
         {
+            _freezingSlider.gameObject.SetActive(_currentFrostbite == 0 ? false: true);
             if (_frostbiteCauses.Count > 0) { _currentFrostbite += (2 * _frostbiteCauses.Count); }
             if (_frostbiteCauses.Count == 0 && _currentFrostbite > 0) { _currentFrostbite--; }
+            _freezingSlider.value = _freezingSlider.maxValue - _currentFrostbite;
 
             if (_currentFrostbite >= FrostbiteDeath) { _playerHP.GameOver(); }
 
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.2f);
         }
     }
     public void ManageFrostbiteCauses(string name, bool remove)

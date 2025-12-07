008888df6b0442a6dcf706b9ea01071353f5b421
Spectre <rpverschuren@st.noorderpoort.nl>
2025-12-07 17:47:33 +0100

    Window sizes

---
+++ Assets/Scripts/Settings/WindowSetting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class WindowSetting : MonoBehaviour
+    {
+        public void SwitchWindowMode(int windowType)
+        {
+            //Self explanatory
+            switch (windowType)
+            {
+                case 0:
+                    Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                    break;
+                case 1:
+                    Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+                    break;
+                case 2:
+                    Screen.fullScreenMode = FullScreenMode.Windowed;
+                    break;
+            }
+        }
+    }
+}

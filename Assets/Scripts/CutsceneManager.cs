bfcac76720de11e7dc94edba35dd8c853deae348
Spectre <rpverschuren@st.noorderpoort.nl>
2026-03-22 04:04:12 +0100

    Cutscenes are easy to use now.

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Playables;
-using UnityEngine.SceneManagement;
 using UnityEngine.Timeline;
 using UnityEngine.UI;
 
@@ -18,83 +16,123 @@ namespace Gameplay
         [SerializeField] private Image _fadeImage;
         [SerializeField] private GameObject _startingCutscene;
         [SerializeField] private TimelineAsset _startingTimeline;
-        [SerializeField] private TimelineAsset _endingTimeline;
         private GameObject _player;
         [SerializeField] private GameObject _backgroundSound;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
 
         private void OnEnable()
         {
-            _input.SkipEvent += HandleSkip;
+            _input.SkipEvent += SkipCutscene;
         }
 
         void Start()
         {
             instance = this;
             _player = PlrRefs.inst.gameObject;
-            if (FindAnyObjectByType<PlayableDirector>())
-            {
-                StartCoroutine(StartStartingCutscene());
-            }
+            StartCutscene(_startingCutscene, _startingTimeline, null, () => { Generation.mainInstance.FastLoading = true; }, () => { _backgroundSound.SetActive(true); },out GameObject scene);
         }
 
-        private void StartCutscene()
+        private GameObject currentAnimPlaying = null;
+        private Action onAnimSkip;
+        private Action onAnimEnd;
+        private bool animEndFreeMouse = true;
+        private int cutsceneID = 0;
+        private bool inCutscene = false;
+        public void StartCutscene(GameObject pref, TimelineAsset timeL, Action onStart, Action onSkip, Action onEnd, out GameObject spawnedScene, bool fadeAtStart = false, bool fadeAtEnd = false, bool freeMouseOnEnd = false, Vector3 animLocation = new(), Quaternion animRotation = new())
         {
-            _cutsceneUi.SetActive(true);
-            _input.SetCutsceneActions();
-            //turn off player
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            _ui.SetActive(false);
-            PlrRefs.inst.PlayerController.enabled = false;
-        }
+            //needs to be anything before a return
+            spawnedScene = null;
 
-        private void EndCutscene(bool freeMouse)
-        {
-            _cutsceneUi.SetActive(false);
-            _input.SetGameplayActions();
-            //turn off player
-            if (!freeMouse)
+            //if there already is an animation or if there is no object
+            if (currentAnimPlaying != null || pref == null || inCutscene) { return; }
+
+            inCutscene = true;
+            //if another cutscene gets started during the waiting time of another, it will know.
+            cutsceneID += 1;
+
+            //Fade based on FadeAtStart
+            StartCoroutine(FadeScreen((fadeAtStart ? 0.3f : 0), (fadeAtStart ? 1 : 0), () =>
             {
+                //Spawn animation
+                if (pref.scene.name == null)
+                {
+                    currentAnimPlaying = Instantiate(pref,animLocation,animRotation);
+                }
+                else
+                {
+                    currentAnimPlaying = pref;
+                }
+                _cutsceneUi.SetActive(true);
+
+                //turn off player
+                _input.SetCutsceneActions();
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
+                _ui.SetActive(false);
+                PlrRefs.inst.PlayerController.enabled = false;
 
-            _ui.SetActive(true);
-            PlrRefs.inst.PlayerController.enabled = true;
+                //These values are set for skipping
+                onAnimSkip = onSkip;
+                onAnimEnd = onEnd;
+                animEndFreeMouse = freeMouseOnEnd;
+
+                if (onStart != null) { onStart(); }
+                StartCoroutine(WaitCutsceneTime(timeL, onEnd, fadeAtEnd, freeMouseOnEnd));
+            }));
+        }
+
+        private IEnumerator WaitCutsceneTime(TimelineAsset timeL, Action onEnd, bool fadeAtEnd, bool freeMouseOnEnd)
+        {
+            //if another cutscene gets started during the waiting time of another, it will know.
+            int thisCutsceneID = cutsceneID;
+
+            yield return new WaitForSeconds((float)timeL.duration - (fadeAtEnd ? 0.1f:0.4f));
+
+            if(cutsceneID != thisCutsceneID) { yield break; }
+            EndCutscene(onEnd, fadeAtEnd, freeMouseOnEnd);
+        }
+
+        private void EndCutscene(Action onEnd, bool fadeAtEnd, bool freeMouseOnEnd)
+        {
+            if(currentAnimPlaying == null) { return; }
+
+            //Fade based on fadeAtEnd
+            StartCoroutine(FadeScreen((fadeAtEnd ? 0.3f : 0), (fadeAtEnd ? 1 : 0), () =>
+            {
+                _cutsceneUi.SetActive(false);
+
+                //turn on player
+                _input.SetGameplayActions();
+                if (!freeMouseOnEnd)
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+
+                _ui.SetActive(true);
+                PlrRefs.inst.PlayerController.enabled = true;
+
+                //reset all cutscene stuff
+                Destroy(currentAnimPlaying);
+                currentAnimPlaying = null;
+                onAnimEnd = null;
+                animEndFreeMouse = true;
+
+                if (onEnd != null) { onEnd(); }
+                inCutscene = false;
+            }));
         }
 
         private void SkipCutscene()
         {
-            //implement later
-        }
-        private IEnumerator StartStartingCutscene()
-        {
-            StartCutscene();
-
-            yield return new WaitForSeconds((float)_startingTimeline.duration - 0.1f);
-
-            StopStartingCutscene(false);
-        }
-
-        public void HandleSkip()
-        {
-            StopStartingCutscene();
-        }
-        public void StopStartingCutscene(bool fade = true)
-        {
-            if (!_startingCutscene.activeSelf) return;
-
-            StartCoroutine(FadeScreen(0.3f * (fade ? 1 : 0), 1 * (fade ? 1 : 0), () => {
-                EndCutscene(false);
-                _backgroundSound.SetActive(true);
-                Generation.mainInstance.FastLoading = true;
-            }));
+            if(onAnimSkip != null) { onAnimSkip(); }
+            onAnimSkip = null;
+            EndCutscene(onAnimEnd,true,animEndFreeMouse);
         }
 
         public IEnumerator FadeScreen(float fadeTime, float darkTime, Action betweenSceneCode = null)
@@ -131,86 +169,62 @@ namespace Gameplay
             _fadeImage.gameObject.SetActive(false);
         }
 
-        /// <summary>
-        /// This function exists to call the function in places you cant start coroutines
-        /// </summary>
-        public void StartEndingCutscene()
-        {
-            StartCoroutine(StartEndingCutsceneCoroutine());
-        }
-
-        public IEnumerator StartEndingCutsceneCoroutine()
-        {
-
-            StartCutscene();
-            yield return new WaitForSeconds((float)_endingTimeline.duration);
-            EndCutscene(true);
-            SceneManager.LoadScene(0);
-        }
-
         public static void StartCutsceneStatic(string name)
         {
-            instance.StartCutscene(name);
+            instance.StartCutsceneByName(name);
         }
 
-        public void StartCutscene(string name)
+        /// <summary>
+        /// Used for cutscenes that cannot be hosted elsewhere, aka homeless cutscenes
+        /// </summary>
+        /// <param name="name"></param>
+        public void StartCutsceneByName(string name)
         {
             switch (name)
             {
                 case "PaintingSecret":
-                    StartCoroutine(StartSecretCutscene());
+                    StartCutscene(_paintingCutscenePrefab, _paintingTimeline, () =>
+                    {
+                        GameObject fakeRoom = PlrRefs.inst.PlayerController.CurrentRoom;
+
+                        //delete all current events
+                        List<EventClass> even = fakeRoom.GetComponent<CarriageClass>()._selectedEventClasses;
+                        for (int i = 0; i < even.Count; i++)
+                        {
+                            even[i].Exited(fakeRoom.GetComponent<CarriageClass>());
+                            even[i].CallForDeletion(fakeRoom.GetComponent<CarriageClass>());
+                        }
+                        _player.GetComponent<Rigidbody>().isKinematic = true;
+
+                        originalRoomLocal = fakeRoom.transform.position;
+                        fakeRoom.transform.position = new Vector3(100, 100, 100);
+                    }, null, () =>
+                    {
+                        PlrRefs.inst.PlayerController.CurrentRoom.transform.position = originalRoomLocal;
+                        _player.GetComponent<Rigidbody>().isKinematic = false;
+
+                        GameObject newGen = Instantiate(_baseGen, new Vector3(200, 0, 200), new Quaternion(0, 0, 0, 0));
+
+                        Generation paintingRoomsGenerator = newGen.GetComponent<Generation>();
+                        paintingRoomsGenerator.player = _player;
+                        paintingRoomsGenerator.AmountOfRooms = 5;
+                        paintingRoomsGenerator.Rooms = _paintingRooms;
+                        paintingRoomsGenerator.FastLoading = true;
+                    }, out GameObject scene, true, true, false, new Vector3(100, 100, 100), new Quaternion(0, 0, 0, 0));
                     break;
             }
         }
 
-
+        //Values for homeless cutscenes
+        [Header("Painting Cutscene")]
         [SerializeField] private GameObject _paintingCutscenePrefab;
         [SerializeField] private TimelineAsset _paintingTimeline;
         [SerializeField] private RoomTypeScriptable _paintingRooms;
         [SerializeField] private GameObject _baseGen;
-        public IEnumerator StartSecretCutscene()
-        {
-            GameObject spawnedScene = null;
-            GameObject fakeRoom = PlrRefs.inst.PlayerController.CurrentRoom;
-
-            //delete all current events
-            List<EventClass> even = fakeRoom.GetComponent<CarriageClass>()._selectedEventClasses;
-            for (int i = 0; i < even.Count; i++)
-            {
-                even[i].Exited(fakeRoom.GetComponent<CarriageClass>());
-                even[i].CallForDeletion(fakeRoom.GetComponent<CarriageClass>());
-            }
-
-            Vector3 originalRoomPos = fakeRoom.transform.position;
-            StartCoroutine(FadeScreen(0.3f, 1, () =>
-            {
-                spawnedScene = Instantiate(_paintingCutscenePrefab, new Vector3(100, 100, 100), new Quaternion(0, 0, 0, 0));
-                PlrRefs.inst.PlayerController.enabled = false;
-                _player.GetComponent<Rigidbody>().isKinematic = true;
-                fakeRoom.transform.position = spawnedScene.transform.position;
-            }));
-
-            yield return new WaitForSeconds((float)(_paintingTimeline.duration) - 0.3f);
-
-            StartCoroutine(FadeScreen(0.3f, 1, () =>
-            {
-                fakeRoom.transform.position = originalRoomPos;
-                PlrRefs.inst.PlayerController.enabled = true;
-                _player.GetComponent<Rigidbody>().isKinematic = false;
-                Destroy(spawnedScene);
-
-                GameObject newGen = Instantiate(_baseGen, new Vector3(200, 0, 200), new Quaternion(0, 0, 0, 0));
-
-                Generation paintingRoomsGenerator = newGen.GetComponent<Generation>();
-                paintingRoomsGenerator.player = _player;
-                paintingRoomsGenerator.AmountOfRooms = 5;
-                paintingRoomsGenerator.Rooms = _paintingRooms;
-                paintingRoomsGenerator.FastLoading = true;
-            }));
-        }
+        private Vector3 originalRoomLocal;
         private void OnDisable()
         {
-            _input.SkipEvent -= HandleSkip;
+            _input.SkipEvent -= SkipCutscene;
         }
     }
 }
--- Assets/Scripts/Events/EndingCutsceneEvent.cs
+++ Assets/Scripts/Events/EndingCutsceneEvent.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
 
 namespace Gameplay
 {
@@ -7,14 +9,13 @@ namespace Gameplay
     {
         [SerializeField]
         private GameObject _endingCutscenePrefab;
+        [SerializeField] private TimelineAsset _endingTimeline;
         public override bool Entered(CarriageClass room)
         {
             //turn off the player and start the cutscene
-            GameObject cut =  Instantiate(_endingCutscenePrefab);
-            cut.transform.position = new Vector3(300, 300, 300);
-            PlrRefs.inst.Camera.transform.parent = cut.transform;
-            PlrRefs.inst.gameObject.SetActive(false);
-            CutsceneManager.instance.StartEndingCutscene();
+            PlrRefs.inst.Camera.transform.parent = CutsceneManager.instance.transform;
+            GameObject scene = null;
+            CutsceneManager.instance.StartCutscene(_endingCutscenePrefab,_endingTimeline,() => { PlrRefs.inst.gameObject.SetActive(false); },null,() => { SceneManager.LoadScene(0); }, out scene, true,true,true, new Vector3(300, 300, 300));
             return true;
         }
         public override bool Exited(CarriageClass room) { return true; }
